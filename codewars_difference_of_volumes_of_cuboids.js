762c939677bd7e19b14815a167ee0882011827d4
Crew Spence <csspence123@gmail.com>
2021-03-13 07:07:32 -0600

    Wrote working solution.

--- codewars_difference_of_volumes_of_cuboids.js
+++ codewars_difference_of_volumes_of_cuboids.js
@@ -8,6 +8,14 @@ Your function will be tested with pre-made examples as well as random ones.
 If you can, try writing it in one line of code.
 */
 
-function findDifference(a, b) {
-  //loading...
+const findDifference = (a, b) => {
+  let first = a[0] * a[1] * a[2];
+  let second = b[0] * b[1] * b[2];
+  if(first > second) {
+    return first - second;
+  } else if(second > first) {
+    return second - first;
+  } else {
+    return 0;
+  }
 }
